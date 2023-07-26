2a0698f57974cb47e177886767509c9bee8ad5dd
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2023-07-26 09:00:19 -0700

    Add And Refactor Web Project

--- src/BlazorBlog.Web/Program.cs
+++ src/BlazorBlog.Web/Program.cs
@@ -1,12 +1,15 @@
 using BlazorBlog.Web;
 
+using Blazored.LocalStorage;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
